f0f4278131a22efdd1ff406b9e7bd96b91a967da
uramsaroop <umesh.ramsaroop@gmail.com>
2026-02-23 19:45:09 +0000

    final updates for backend

--- barely-surviving/backend/app/models/weight.py
+++ barely-surviving/backend/app/models/weight.py
@@ -1,11 +1,12 @@
 from beanie import Document
 from pydantic import Field
-from datetime import datetime, date
+from datetime import datetime
+from datetime import date as date_type
 
 
 class Weight(Document):
     weight: float = Field(..., description="Weight in pounds")
-    date: date = Field(default_factory=lambda: datetime.utcnow().date(), description="Weight measurement date")
+    date: date_type = Field(default_factory=lambda: datetime.utcnow().date(), description="Weight measurement date")
     created_at: datetime = Field(default_factory=datetime.utcnow)
     
     class Settings:
